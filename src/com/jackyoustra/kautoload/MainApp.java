57d6e6f857d528b67bfe2c50c57f36a0889882f5
Jack Youstra <jack@youstra.com>
2015-02-11 17:19:13 -0800

    Added classpath; Made tab bar multi-paned, action & info; Actions don't work, only there for show. They're disabled on kindle removal.

--- src/com/jackyoustra/kautoload/MainApp.java
+++ src/com/jackyoustra/kautoload/MainApp.java
@@ -19,9 +19,11 @@ import javax.swing.JOptionPane;
 import javax.swing.ListModel;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileSystemView;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainApp {
 
@@ -32,6 +34,9 @@ public class MainApp {
 	private JLabel lblBooks;
 	private JTabbedPane tabbedPane;
 	private JPanel infopanel;
+	private JLabel lblSpace;
+	private JPanel actionPanel;
+	private JButton btnDownload;
 
 	/**
 	 * Launch the application.
@@ -62,10 +67,10 @@ public class MainApp {
 	private void initialize() {
 
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 454, 297);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 434, 0, 0, 0 };
+		gridBagLayout.columnWidths = new int[] { 434, -108, 150, 0 };
 		gridBagLayout.rowHeights = new int[] { 14, 0, 0, 248, 0 };
 		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 0.0, Double.MIN_VALUE };
 		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE };
@@ -76,7 +81,7 @@ public class MainApp {
 		GridBagConstraints gbc_lblKindleStatus = new GridBagConstraints();
 		gbc_lblKindleStatus.anchor = GridBagConstraints.NORTH;
 		gbc_lblKindleStatus.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblKindleStatus.insets = new Insets(0, 0, 5, 5);
+		gbc_lblKindleStatus.insets = new Insets(5, 0, 5, 5);
 		gbc_lblKindleStatus.gridx = 0;
 		gbc_lblKindleStatus.gridy = 0;
 		frame.getContentPane().add(lblKindleStatus, gbc_lblKindleStatus);
@@ -109,6 +114,41 @@ public class MainApp {
 		
 		infopanel = new JPanel();
 		tabbedPane.addTab("Info", null, infopanel, null);
+		GridBagLayout gbl_infopanel = new GridBagLayout();
+		gbl_infopanel.columnWidths = new int[]{36, 46, 0};
+		gbl_infopanel.rowHeights = new int[]{14, 0, 0};
+		gbl_infopanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_infopanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		infopanel.setLayout(gbl_infopanel);
+		
+		lblSpace = new JLabel("Space");
+		GridBagConstraints gbc_lblSpace = new GridBagConstraints();
+		gbc_lblSpace.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblSpace.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSpace.gridx = 0;
+		gbc_lblSpace.gridy = 0;
+		infopanel.add(lblSpace, gbc_lblSpace);
+		
+		actionPanel = new JPanel();
+		tabbedPane.addTab("Actions", null, actionPanel, null);
+		GridBagLayout gbl_actionPanel = new GridBagLayout();
+		gbl_actionPanel.columnWidths = new int[]{79, 0};
+		gbl_actionPanel.rowHeights = new int[]{23, 0};
+		gbl_actionPanel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_actionPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		actionPanel.setLayout(gbl_actionPanel);
+		
+		btnDownload = new JButton("Download");
+		btnDownload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnDownload = new GridBagConstraints();
+		gbc_btnDownload.insets = new Insets(5, 5, 0, 0);
+		gbc_btnDownload.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnDownload.gridx = 0;
+		gbc_btnDownload.gridy = 0;
+		actionPanel.add(btnDownload, gbc_btnDownload);
 		
 		// constantly check for kindle
 		ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
@@ -169,12 +209,19 @@ public class MainApp {
 
 	private void onKindleDisconnected() {
 		lblKindleStatus.setText("Kindle not connected!");
+		BookList.setSelectedIndex(-1);
 		BookList.setEnabled(false);
+		lblSpace.setText("No kindle connected");
+		tabbedPane.setSelectedIndex(0);
+		tabbedPane.setEnabledAt(0, false);
+		tabbedPane.setEnabledAt(1, false);
 	}
 
 	private void onKindleConnected() {
 		lblKindleStatus.setText("Kindle connected");
 		BookList.setEnabled(true);
+		tabbedPane.setEnabledAt(0, true);
+		tabbedPane.setEnabledAt(1, true);
 	}
 
 	private void listenForKindle() {
@@ -183,7 +230,8 @@ public class MainApp {
 		boolean success = false;
 		for (int i = 0; i < f.length; i++) {
 			// check if the drive is a kindle
-			if (fsv.getSystemDisplayName(f[i]).contains("Kindle")
+			
+			if (fsv.getSystemDisplayName(f[i]).toLowerCase().contains("kindle")
 					&& fsv.isDrive(f[i]) && f[i].canRead() && f[i].canWrite()) {
 				// bet bet that it is
 				success = true;
